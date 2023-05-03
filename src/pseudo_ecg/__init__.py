f9f0607a4f2dfbe5146c468dac43ed5303aad4ec
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-05-03 13:44:37 +0200

    Add some utility functions for dealing with mesviews

--- src/pseudo_ecg/__init__.py
+++ src/pseudo_ecg/__init__.py
@@ -2,6 +2,7 @@ from importlib.metadata import metadata
 
 from . import ecg
 from . import eikonal
+from . import mesh_utils
 
 meta = metadata("pseudo-ecg")
 __version__ = meta["Version"]
@@ -10,4 +11,4 @@ __license__ = meta["License"]
 __email__ = meta["Author-email"]
 __program_name__ = meta["Name"]
 
-__all__ = ["ecg", "eikonal"]
+__all__ = ["ecg", "eikonal", "mesh_utils"]
