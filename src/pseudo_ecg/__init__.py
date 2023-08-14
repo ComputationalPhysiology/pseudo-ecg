06a92701f10c8f9e02c1fe43ed42c5c6f3e53a5b
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-08-14 13:04:06 +0200

    One slab demo to rule them all

--- src/pseudo_ecg/__init__.py
+++ src/pseudo_ecg/__init__.py
@@ -4,6 +4,13 @@ from . import ecg
 from . import eikonal
 from . import mesh_utils
 
+from .mesh_utils import (
+    surface_to_volume_ratio,
+    vertex_map_kdtree,
+    vertex_map_meshview,
+    vertex_map_submesh,
+)
+
 meta = metadata("pseudo-ecg")
 __version__ = meta["Version"]
 __author__ = meta["Author"]
@@ -11,4 +18,12 @@ __license__ = meta["License"]
 __email__ = meta["Author-email"]
 __program_name__ = meta["Name"]
 
-__all__ = ["ecg", "eikonal", "mesh_utils"]
+__all__ = [
+    "ecg",
+    "eikonal",
+    "mesh_utils",
+    "surface_to_volume_ratio",
+    "vertex_map_kdtree",
+    "vertex_map_meshview",
+    "vertex_map_submesh",
+]
