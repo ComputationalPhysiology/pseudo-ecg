136cc711d8835ce0caff36e67ad430924fb2123c
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-08-25 18:11:58 +0200

    Attempt to add pseudo-bidomain for biv demo

--- demos/biv/main.py
+++ demos/biv/main.py
@@ -26,11 +26,11 @@ def harmonic_mean(a, b):
 def define_stimulus(mesh, time, chi, C_m) -> cbcbeat.Markerwise:
     # Define some external stimulus
     S1_marker = 1
-    S1_subdomain = dolfin.CompiledSubDomain("x[0] <= 0.01")
+    S1_subdomain = dolfin.CompiledSubDomain("x[0] > 3.5")
     S1_markers = dolfin.MeshFunction("size_t", mesh, mesh.topology().dim())
     S1_subdomain.mark(S1_markers, S1_marker)
 
-    amp = 50000.0  # mu A/cm^3
+    amp = 500.0  # mu A/cm^3
     factor = 1.0 / (chi * C_m)  # NB: cbcbeat convention
 
     amplitude = factor * amp  # mV/ms
@@ -44,21 +44,6 @@ def define_stimulus(mesh, time, chi, C_m) -> cbcbeat.Markerwise:
     )
 
     return cbcbeat.Markerwise((I_s,), (S1_marker,), S1_markers)
-
-
-def define_fibers():
-    fiber = dolfin.as_vector([1, 0, 0])
-    sheet = dolfin.as_vector([0, 1, 0])
-    cross_sheet = dolfin.as_vector([0, 0, 1])
-
-    A = dolfin.as_matrix(
-        [
-            [fiber[0], sheet[0], cross_sheet[0]],
-            [fiber[1], sheet[1], cross_sheet[1]],
-            [fiber[2], sheet[2], cross_sheet[2]],
-        ]
-    )
-    return A
 
 
 def load_from_file(heart_mesh, xdmffile, key="v"):
@@ -91,84 +76,60 @@ class Conductivites(NamedTuple):
 
 
 def setup_conductivites(
-    chi, C_m, g_il=0.34, g_it=0.060, g_el=0.12, g_et=0.080, g_b=1.0
-):
-    g_bulk_l = harmonic_mean(g_il, g_el)
-    g_bulk_t = harmonic_mean(g_it, g_et)
-
-    A = define_fibers()
-
-    factor = 1 / (chi * C_m)
-
-    M_star = ufl.diag(
-        dolfin.as_vector(np.array([g_bulk_l, g_bulk_t, g_bulk_t]) * factor)
-    )
-    G_m = A * M_star * A.T
-
-    M_e_star = ufl.diag(dolfin.as_vector(np.array([g_el, g_et, g_et]) * factor))
-    M_i_star = ufl.diag(dolfin.as_vector(np.array([g_il, g_it, g_it]) * factor))
-    G_e = A * M_e_star * A.T
-    G_i = A * M_i_star * A.T
-
-    return Conductivites(
-        G_i=G_i,
-        G_e=G_e,
-        G_m=G_m,
-        g_il=g_il * factor,
-        g_it=g_it * factor,
-        g_el=g_el * factor,
-        g_et=g_et * factor,
-        g_b=g_b * factor,
-    )
-
-
-def setup_augmented_conductivites(
     chi,
     C_m,
-    mesh,
-    augmented_bath_size=0.01,
+    A,
     g_il=0.34,
     g_it=0.060,
     g_el=0.12,
     g_et=0.080,
     g_b=1.0,
+    mesh=None,
+    use_augmented_monodomain: bool = False,
+    augmented_bath_size=0.01,
 ):
     g_bulk_l = harmonic_mean(g_il, g_el)
     g_bulk_t = harmonic_mean(g_it, g_et)
 
-    g_edge_l = harmonic_mean(g_il, g_b)
-    g_edge_t = harmonic_mean(g_it, g_b)
-
-    bath_aug_marker = 1
-    bath_aug_subdomain = dolfin.CompiledSubDomain(
-        "x[2] < s + DOLFIN_EPS || x[2] > 0.5 - s - DOLFIN_EPS", s=augmented_bath_size
-    )
-    bath_aug_markers = dolfin.MeshFunction("size_t", mesh, mesh.topology().dim())
-    bath_aug_markers.set_all(0)
-    bath_aug_subdomain.mark(bath_aug_markers, bath_aug_marker)
-
-    A = define_fibers()
-
     factor = 1 / (chi * C_m)
 
-    edge_inds = np.where(bath_aug_markers.array() == 1)[0]
-    bulk_inds = np.where(bath_aug_markers.array() == 0)[0]
+    if use_augmented_monodomain:
+        raise NotImplementedError
+        g_edge_l = harmonic_mean(g_il, g_b)
+        g_edge_t = harmonic_mean(g_it, g_b)
 
-    V_g = dolfin.FunctionSpace(mesh, "DG", 0)
-    g_bulk_l_fun = dolfin.Function(V_g)
-    g_bulk_l_fun.vector()[bulk_inds] = g_bulk_l * factor
-    g_bulk_l_fun.vector()[edge_inds] = g_edge_l * factor
+        bath_aug_marker = 1
+        bath_aug_subdomain = dolfin.CompiledSubDomain(
+            "x[2] < s + DOLFIN_EPS || x[2] > 0.5 - s - DOLFIN_EPS",
+            s=augmented_bath_size,
+        )
+        bath_aug_markers = dolfin.MeshFunction("size_t", mesh, mesh.topology().dim())
+        bath_aug_markers.set_all(0)
+        bath_aug_subdomain.mark(bath_aug_markers, bath_aug_marker)
 
-    g_bulk_t_fun = dolfin.Function(V_g)
-    g_bulk_t_fun.vector()[bulk_inds] = g_bulk_t * factor
-    g_bulk_t_fun.vector()[edge_inds] = g_edge_t * factor
+        edge_inds = np.where(bath_aug_markers.array() == 1)[0]
+        bulk_inds = np.where(bath_aug_markers.array() == 0)[0]
 
-    M_star = ufl.diag(dolfin.as_vector([g_bulk_l_fun, g_bulk_t_fun, g_bulk_t_fun]))
+        V_g = dolfin.FunctionSpace(mesh, "DG", 0)
+        g_bulk_l_fun = dolfin.Function(V_g)
+        g_bulk_l_fun.vector()[bulk_inds] = g_bulk_l * factor
+        g_bulk_l_fun.vector()[edge_inds] = g_edge_l * factor
+
+        g_bulk_t_fun = dolfin.Function(V_g)
+        g_bulk_t_fun.vector()[bulk_inds] = g_bulk_t * factor
+        g_bulk_t_fun.vector()[edge_inds] = g_edge_t * factor
+
+        M_star = ufl.diag(dolfin.as_vector([g_bulk_l_fun, g_bulk_t_fun, g_bulk_t_fun]))
+
+    else:
+        M_star = ufl.diag(
+            dolfin.as_vector(np.array([g_bulk_l, g_bulk_t, g_bulk_t]) * factor)
+        )
 
     G_m = A * M_star * A.T
 
-    M_e_star = ufl.diag(dolfin.as_vector([g_el, g_et, g_et]))
-    M_i_star = ufl.diag(dolfin.as_vector([g_il, g_it, g_it]))
+    M_e_star = ufl.diag(dolfin.as_vector(np.array([g_el, g_et, g_et]) * factor))
+    M_i_star = ufl.diag(dolfin.as_vector(np.array([g_il, g_it, g_it]) * factor))
     G_e = A * M_e_star * A.T
     G_i = A * M_i_star * A.T
 
@@ -249,6 +210,7 @@ def compute_phie_recovery(
     electrodes: list[tuple[float, float, float]], mesh, g_b, vs: list[dolfin.Function]
 ) -> dict[tuple[float, float, float], list[float]]:
     ecg = defaultdict(list)
+    print("Compute ecg recovert")
     for v in vs:
         for el in electrodes:
             ecg[el].append(
@@ -283,28 +245,27 @@ def main(
     # Bath conductivit
     g_b = 1.0  # S / m
 
-    if use_augmented_monodomain:
-        conductivites = setup_augmented_conductivites(
-            chi=chi,
-            C_m=C_m,
-            mesh=heart,
-            augmented_bath_size=0.01,
-            g_il=g_il,
-            g_it=g_it,
-            g_el=g_el,
-            g_et=g_et,
-            g_b=g_b,
-        )
-    else:
-        conductivites = setup_conductivites(
-            chi=chi,
-            C_m=C_m,
-            g_il=g_il,
-            g_it=g_it,
-            g_el=g_el,
-            g_et=g_et,
-            g_b=g_b,
-        )
+    A = dolfin.as_matrix(
+        [
+            [geo.f0[0], geo.s0[0], geo.n0[0]],
+            [geo.f0[1], geo.s0[1], geo.n0[1]],
+            [geo.f0[2], geo.s0[2], geo.n0[2]],
+        ]
+    )
+
+    conductivites = setup_conductivites(
+        chi=chi,
+        C_m=C_m,
+        mesh=heart,
+        A=A,
+        augmented_bath_size=0.01,
+        g_il=g_il,
+        g_it=g_it,
+        g_el=g_el,
+        g_et=g_et,
+        g_b=g_b,
+        use_augmented_monodomain=use_augmented_monodomain,
+    )
 
     time = dolfin.Constant(0.0)
 
@@ -327,19 +288,39 @@ def main(
     ]
 
     # Indices
-    left_arm = 1
-    right_arm = 2
-    left_leg = 3
+    left_arm_index = 1
+    right_arm_index = 2
+    left_leg_index = 3
 
     if use_pseudobidomain:
+        A_torso = dolfin.as_matrix(
+            [
+                [geo_torso.f0[0], geo_torso.s0[0], geo_torso.n0[0]],
+                [geo_torso.f0[1], geo_torso.s0[1], geo_torso.n0[1]],
+                [geo_torso.f0[2], geo_torso.s0[2], geo_torso.n0[2]],
+            ]
+        )
+        conductivites_torso = setup_conductivites(
+            chi=chi,
+            C_m=C_m,
+            mesh=geo_torso.mesh,
+            A=A_torso,
+            augmented_bath_size=0.01,
+            g_il=g_il,
+            g_it=g_it,
+            g_el=g_el,
+            g_et=g_et,
+            g_b=g_b,
+            use_augmented_monodomain=use_augmented_monodomain,
+        )
         phie = pseudo_ecg.ecg.pseudo_bidomain(
             mesh=geo_torso.mesh,
             cfun=geo_torso.cfun,
-            heart_marker=geo_torso.markers["Myocardium"][0],
-            torso_marker=geo_torso.markers["Bath"][0],
-            G_i=conductivites.G_i,
-            G_e=conductivites.G_e,
-            g_b=conductivites.g_b,
+            heart_marker=geo_torso.markers["HEART"][0],
+            torso_marker=geo_torso.markers["TISSUE"][0],
+            G_i=conductivites_torso.G_i,
+            G_e=conductivites_torso.G_e,
+            g_b=conductivites_torso.g_b,
             vs=vs,
             electrodes=electrodes,
         )
@@ -350,9 +331,15 @@ def main(
             electrodes=electrodes, mesh=heart, g_b=conductivites.g_b, vs=vs
         )
 
-    lead1 = np.subtract(phie[left_arm], phie[right_arm])
-    lead2 = np.subtract(phie[left_leg], phie[right_arm])
-    lead3 = np.subtract(phie[left_leg], phie[left_arm])
+    lead1 = np.subtract(
+        phie[electrodes[left_arm_index]], phie[electrodes[right_arm_index]]
+    )
+    lead2 = np.subtract(
+        phie[electrodes[left_leg_index]], phie[electrodes[right_arm_index]]
+    )
+    lead3 = np.subtract(
+        phie[electrodes[left_leg_index]], phie[electrodes[left_arm_index]]
+    )
 
     fig, ax = plt.subplots(1, 3, figsize=(12, 8), sharex=True, sharey=True)
     ax[0].plot(lead1)
@@ -373,10 +360,10 @@ def main(
 
 if __name__ == "__main__":
     for use_augmented_monodomain, use_pseudobidomain, xdmffile, fibpath in [
-        (False, False, "out_mono.xdmf", "mono.png"),
+        # (False, False, "out_mono.xdmf", "mono.png"),
         (False, True, "out_mono.xdmf", "pb_mono.png"),
-        (True, False, "out_aug_mono.xdmf", "aug_mono.png"),
-        (True, True, "out_aug_mono.xdmf", "pb_aug_mono.png"),
+        # (True, False, "out_aug_mono.xdmf", "aug_mono.png"),
+        # (True, True, "out_aug_mono.xdmf", "pb_aug_mono.png"),
     ]:
         main(
             use_augmented_monodomain=use_augmented_monodomain,
